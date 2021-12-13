fcf9f25c583d608a6f529caf5847d743b02a7572
centek134 <centek134@wp.pl>
2021-12-13 17:58:10 +0100

    first scratches for computer component

--- src/components/House.tsx
+++ src/components/House.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Hand } from '../containers/BlackJack';
+interface Props {
+    deckId: string;
+    houseHand: Hand["card"];
+    setHouseHand: React.Dispatch<React.SetStateAction<Hand["card"]>>;
+}
 
-export const House = () => {
+export const House: React.FC<Props> = ({houseHand, setHouseHand, deckId}) => {
+    useEffect(() => {
+        if(deckId){
+
+            fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`, {
+                method:"GET"
+            })
+            .then( response => response.json())
+        .then( data => {
+                console.log(data.cards);
+                setHouseHand(data.cards);
+            })
+        .catch( err => console.log(err));
+        
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[deckId])
     return (
-        <div>
-            
-        </div>
+        <fieldset>
+            <legend>House cards</legend>
+            {houseHand? houseHand.map(card => {
+                return(
+                    <img key={card.code} alt='card' src={card.image}/>
+                )
+            }) : null}
+        </fieldset>
     )
 };
 export default House;
