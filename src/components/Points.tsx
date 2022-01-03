ac426ae93d6d91070353bcce6594da8eb39158fc
centek134 <centek134@wp.pl>
2022-01-03 17:33:22 +0100

    changes

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Hand } from "../containers/BlackJack";
 interface Cards {
   hand: Hand["card"];
