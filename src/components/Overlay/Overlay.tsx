47197376911039ac59421ca5c885906412f38e78
centek134 <centek134@wp.pl>
2022-02-07 18:34:25 +0100

    added house & player points, to display after game

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import "./Overlay.css";
 interface Props {
     winner:string,
-    gameReset: () => void
+    gameReset: () => void,
+    housePoints: number,
+    playerPoints:number
 }
-export const Overlay : React.FC<Props> = ({winner, gameReset}) => {
+export const Overlay : React.FC<Props> = ({winner, gameReset, housePoints, playerPoints}) => {
     return (
         <div className="overlay">
             <div className="popup">
                 <h2>{winner}</h2>
+                <p>House points: {housePoints}</p>
+                <p>Player points: {playerPoints}</p>
                 <button onClick={gameReset}>Next round!</button>
             </div>
             
